0532a2f4a9acd225d9f1c6428e75bb42dce9d71f
Елена Лебедева <arkadi.18.18@mail.ru>
2022-01-24 15:28:38 +0300

    final

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,20 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
-}
+    let data = {goods:[],order:{}};
+  
+    let countOfGoods = goods.length;  
+   
+     data.client = name + ' ' + phone;
+     data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+     data.order.sum = sum; 
+  
+     for (let i = 0; i < countOfGoods; i += 1) {
+           let tmp = {title: goods[i].title, count: goods[i].count}
+           data.goods.push(tmp);
+     }
+  
+     let jsonData = JSON.stringify({data});
+  
+     return jsonData;
+  }
